3d91cc705b3b476b67784c48466b0452d6e9d350
WHIZBOT-Md <167946140+WHIZBOT1@users.noreply.github.com>
2024-04-27 18:40:59 -0500

    Update Info.ts

--- src/Commands/General/Info.ts
+++ src/Commands/General/Info.ts
@@ -10,11 +10,8 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class extends BaseCommand {
     private imageUrls: string[] = [
-        'https://telegra.ph/file/96e86a5fe768fb5cc6193.jpg',
-        'https://telegra.ph/file/78e38a311fb321471d83c.jpg',
-        'https://telegra.ph/file/41964deb11222c107996e.jpg',
-        'https://telegra.ph/file/8b87b410b49d61de535d3.jpg',
-        'https://telegra.ph/file/e9aa5e407abe6d7911ace.jpg',
+        'https://telegra.ph/file/c629c94e5e66e01b5dd67.jpg',
+      
     ]
     // you can add more pictures if you want bro or girl
     public override execute = async ({ reply }: Message): Promise<void> => {
